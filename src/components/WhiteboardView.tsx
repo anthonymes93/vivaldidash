a5def5ff7e20caf7e41c08fc5c4f44de55183440
Tony <anthonymeszaros93@gmail.com>
2026-05-05 07:23:20 -0400

    refactor: replace tldraw with excalidraw to fix vercel crashes

--- src/components/WhiteboardView.tsx
+++ src/components/WhiteboardView.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Tldraw } from 'tldraw';
-import 'tldraw/tldraw.css';
+import { Excalidraw } from '@excalidraw/excalidraw';
 
 const WhiteboardView: React.FC = () => {
   return (
     <div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, zIndex: 5 }}>
-      <Tldraw 
-        persistenceKey="vivaldidash-test-whiteboard"
-        inferDarkMode={true}
+      <Excalidraw 
+        theme="dark" 
+        UIOptions={{
+          canvasActions: {
+            changeViewBackgroundColor: true,
+            clearCanvas: true,
+            loadScene: true,
+            saveToActiveFile: true,
+            toggleTheme: true,
+            saveAsImage: true
+          }
+        }}
       />
       <style>{`
-        .tl-ui-layout {
+        .excalidraw {
+          --color-background-default: #0f0f13 !important;
+        }
+        /* Push the UI down so it doesn't overlap with the TopBar */
+        .App-menu {
+          margin-top: 70px !important;
+        }
+        .layer-ui__wrapper {
           padding-top: 70px !important;
         }
       `}</style>
